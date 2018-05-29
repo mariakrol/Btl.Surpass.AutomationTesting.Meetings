26487ddde8761d2a1f9620e715624b308a54cfb2
maria.krol <mariak@saltswharf.btl.com>
2018-05-29 12:36:08 +0300

    Meeting 2: Add behavior for vehicle

--- Meeting2.LearnOop/Program.cs
+++ Meeting2.LearnOop/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Meeting2.LearnOop
+﻿using System;
+
+namespace Meeting2.LearnOop
 {
     internal class Program
     {
-        private static void Main()
+        public static void Main()
         {
+            var primitiveVehicle = new Vehicle(5);
+
+            Console.WriteLine($"Current speed: {primitiveVehicle.IncreaseSpeed(10)}");
+
+            Console.WriteLine($"Max weight of vehicle: {Vehicle.GetMaxWeight()}");
         }
     }
 }
---
+++ Meeting2.LearnOop/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿namespace Meeting2.LearnOop
+{
+    internal class Vehicle
+    {
+        private int _speed;
+
+        private static readonly int MaxWeight;
+
+        static Vehicle()
+        {
+            MaxWeight = 1000;
+        }
+
+        public Vehicle(int speed)
+        {
+            _speed = speed;
+        }
+
+        public int IncreaseSpeed(int delta)
+        {
+            _speed += delta;
+
+            return _speed;
+        }
+
+        public static int GetMaxWeight() => MaxWeight;
+    }
+}

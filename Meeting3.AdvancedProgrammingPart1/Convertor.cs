74b58014431cf4874c34551f70040a902694af64
maria.krol <mariak@saltswharf.btl.com>
2018-06-05 11:58:23 +0300

    Meeting 3: Prepare classes

--- Meeting3.AdvancedProgrammingPart1/Convertor.cs
+++ Meeting3.AdvancedProgrammingPart1/Convertor.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Meeting3.AdvancedProgrammingPart1
+﻿namespace Meeting3.AdvancedProgrammingPart1
 {
     internal class Convertor
     {
+        public int ConvertDoubleToInt(double sourceValue)
+        {
+            if (sourceValue > int.MaxValue || sourceValue < int.MinValue)
+            {
+                //error?
+            }
 
+            return (int) sourceValue;
+        }
     }
 }
---
+++ Meeting3.AdvancedProgrammingPart1/Garage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Meeting3.AdvancedProgrammingPart1.VehicleParts;
+
+namespace Meeting3.AdvancedProgrammingPart1
+{
+    internal class Garage
+    {
+        public Garage()
+        {
+            Staff = new DriversStaff();
+        }
+
+        public void AssignDriverToVehicle(int driverId, ITransmission vehicleTransmission)
+        {
+            var driver = Staff.GetDriver(driverId);
+
+            if (!vehicleTransmission.GetType().IsInstanceOfType(driver.UsedTransmission))
+            {
+                //error?
+            }
+
+            BusyDrivers.Add(driver);
+
+        }
+
+        private List<Driver> BusyDrivers { get; } = new List<Driver>();
+
+        private DriversStaff Staff { get; }
+    }
+}

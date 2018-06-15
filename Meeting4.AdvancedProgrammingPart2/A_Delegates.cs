eb5f8e354ff21c11f9d28a82b8df38b0eb2fdf8c
maria.krol <mariak@saltswharf.btl.com>
2018-06-15 13:18:06 +0300

    Meeting 4: Add samples for delegetes #2

--- Meeting4.AdvancedProgrammingPart2/A_Delegates.cs
+++ Meeting4.AdvancedProgrammingPart2/A_Delegates.cs
@@ -1,7 +1,42 @@
-﻿namespace Meeting4.AdvancedProgrammingPart2
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Meeting4.AdvancedProgrammingPart2
 {
+    [TestFixture]
     // ReSharper disable once InconsistentNaming
     internal static class A_Delegates
     {
+
+        /// <summary>
+        /// The main thing, you can see in the sample: when we call an instance of delegate IntOperation, a method
+        /// which is referenced by this delegate is called. 
+        /// That's why, the call of a method is checked at runtime, not at compile time.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        [TestCase(5, 10)]
+        public static void A_DelegatecontainReferenceOnMethod(int i, int j)
+        {
+
+            // Here ReSharper tells us, that it is redundant call delegate's constructor, because we can simply assign the method
+            // But we want to see this syntax now
+            // ReSharper disable once RedundantDelegateCreation
+            IntOperation operationWithIntegers = new IntOperation(SimpleCalculator.Add);
+
+            var resultSum = operationWithIntegers(i, j);
+            resultSum
+                .Should()
+                .Be(i + j);
+
+
+            operationWithIntegers = SimpleCalculator.Multiply;
+
+            var resultProduct = operationWithIntegers(i, j);
+            resultProduct
+                .Should()
+                .Be(i * j);
+        }
+
     }
 }
--- Meeting4.AdvancedProgrammingPart2/SimpleCalculator.cs
+++ Meeting4.AdvancedProgrammingPart2/SimpleCalculator.cs
@@ -1,11 +1,31 @@
-﻿namespace Meeting4.AdvancedProgrammingPart2
+﻿using System.Diagnostics;
+
+namespace Meeting4.AdvancedProgrammingPart2
 {
     internal static class SimpleCalculator
     {
-        internal static int Add(int x, int y) => x + y;
+        internal static int Add(int i, int j)
+        {
+            var sum = i + j;
+            Trace.TraceInformation($"Sum of {i} and {j} is {sum}.");
 
-        internal static int Multiply(int x, int y) => x * y;
+            return sum;
+        }
 
-        internal static int Divide(int x, int y) => x / y;
+        internal static int Multiply(int i, int j)
+        {
+            var product = i * j;
+            Trace.TraceInformation($"Product of {i} and {j} is {product}.");
+
+            return product;
+        }
+
+        internal static int Divide(int i, int j)
+        {
+            var quotient = i / j;
+            Trace.TraceInformation($"Quotient  of {i} and {j} is {quotient}.");
+
+            return quotient;
+        }
     }
 }

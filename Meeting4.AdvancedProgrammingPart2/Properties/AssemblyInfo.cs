7ef2889e055829cc7ca49b48b5ae92300ea91bbd
maria.krol <mariak@saltswharf.btl.com>
2018-06-15 12:44:36 +0300

    Meeting 4: Rename Project

--- Meeting4.AdvancedProgrammingPart2/Properties/AssemblyInfo.cs
+++ Meeting4.AdvancedProgrammingPart2/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Meeting3.AdvancedProgrammingPart2")]
+[assembly: AssemblyTitle("Meeting4.AdvancedProgrammingPart2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Meeting3.AdvancedProgrammingPart2")]
+[assembly: AssemblyProduct("Meeting4.AdvancedProgrammingPart2")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

9be7d382d2d47f9b8188238edca1835b26fe0ec0
maria.krol <mariak@saltswharf.btl.com>
2018-07-03 09:48:33 +0300

    Meeting 5: Prepare project with packages

---
+++ Meeting5.IntroductionToAutomatedTesting/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meeting5.IntroductionToAutomatedTesting
+{
+    public class Class1
+    {
+    }
+}
